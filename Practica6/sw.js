e634634167258d0a973f0e709025a8284dd7d773
Gandy Esaú Ávila Estrada <20193tn128@utez.edu.mx>
2022-10-11 14:39:29 -0500

    Práctica 6: Estrategía primero cache y después Network

--- Practica6/sw.js
+++ Practica6/sw.js
@@ -1,5 +1,5 @@
 const INIT_LOG_MSG = 'SW:';
-const ROOT_PATH = "/10A-PWA/Practica5";
+const ROOT_PATH = "/10A-PWA/Practica6";
 const STATIC_CACHE_NAME = 'static-cache-v1.1';
 const INMUTABLE_CACHE_NAME = 'inmutable-cache-v1.0';
 const DYNAMIC_CACHE_NAME = 'dynamic-cache-v1.1';
@@ -35,42 +35,26 @@ self.addEventListener('install', event => {
     ]));
 });
 
-// Estrategía Cache With Network fallback
+// nertwork with cache fallback
 self.addEventListener('fetch', (event) => {
-    const response = caches.match(event.request)
-        .then((respCache) => {
-            // Si esta en cache, devuelvelo
-            // El tiempo de respuesta es realmente reducido a menos de 10 ms
-            if (respCache) return respCache;
 
-            // Si no encontro el cache, entonces ve a la web a buscarlo
-            return fetch(event.request)
-                .then((respWeb) => {
-                    caches.open(DYNAMIC_CACHE_NAME)
-                        .then(async (cache) => {
-                            // Guardalo en cache relacionado con una promesa
-                            // Si llega esa promesa, entonces respondela con otra
-                            // Primero guarda y después borras
-                            await cache.put(event.request, respWeb);
-                            cleanCache(DYNAMIC_CACHE_NAME, 2);
-                        });
-                    
-                    // Es necesario clonar la respuesta para responderla a cache y la otra al fetch
-                    // Un mismo response no puede ser enviado a dos diferentes peticiones
-                    return respWeb.clone();
-                });
+    const response = fetch(event.request).then((respWeb) => {
+        // En caso de que no exista una respuesta, entonces bucamos en cache 
+        if (!respWeb) {
+            return caches.match(event.request);
+        }
+
+        caches.open(DYNAMIC_CACHE_NAME).then(async (cache) => {
+            await cache.put(event.request, respWeb);
+            cleanCache(DYNAMIC_CACHE_NAME, 3);
         });
+        return respWeb.clone();
+    }).catch(() => {
+        return caches.match(event.request);
+    });
 
-    return event.respondWith(response);
+    event.respondWith(response);
 });
-
-/*
-// Estrategía: Only Cache
-self.addEventListener('fetch', event => {
-    const respCache = caches.match(event.request);
-    event.respondWith(respCache);
-});
-*/
 
 // Ayuda a eliminar elementos poniendo un número límite de objetos a guardar
 function cleanCache (cacheName, numberItems) {
