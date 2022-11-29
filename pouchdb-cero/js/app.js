1ceed2aabc51adbecfc2eb52a5246284b8b03f29
GandyA23 <avila.gandy@gmail.com>
2022-11-29 11:03:00 -0600

    Se agrega práctica de manejo de IndexDB con pouchDB

--- pouchdb-cero/js/app.js
+++ pouchdb-cero/js/app.js
@@ -4,36 +4,73 @@ if (navigator.serviceWorker) {
   navigator.serviceWorker.register(`${ROOT_PATH}/sw.js`);
 }
 
-document.getElementById("buttonCreate").addEventListener("click", (event) => {
-  createDatabase();
+let db;
 
+const createDatabase = (event) => {
+  db = new PouchDB('people');  
+  return db;
+}
+
+const store = (name, lastname, hours) => {
+    let person = {
+        _id: new Date().toISOString(),
+        name: name,
+        lastname: lastname,
+        hours: hours,
+        sync: false
+    };
+
+    return db.put(person);
+}
+
+const getAll = () => {
+    return db.allDocs({include_docs: true, descending: true});
+}
+
+const update = (person) => {
+    return db.put(person);
+}
+
+const deleteP = (person) => {
+    return db.remove(person);
+}
+
+document.getElementById("buttonCreate").addEventListener("click", createDatabase);
+document.getElementById("buttonRegister").addEventListener("click", (event) => {
   store(
-    document.getElementById("inputName").ariaValueMax,
-    document.getElementById("inputLastName").ariaValueMax,
-    document.getElementById("inputHours").ariaValueMax,
+    document.getElementById("inputName").value,
+    document.getElementById("inputLastName").value,
+    document.getElementById("inputHours").value,
   ).then((resultStore) => {
     console.log(resultStore);
-
-    // Obten todos los registros
-    getAll().then(async (resultGetAll) => {
-      console.log(resultGetAll.rows);
-
-      // Actualiza todos los registros en sync: true
-      for await(const person of resultGetAll.rows) {
-        const resultUpdateAll = await updateAll(person);
-
-        console.log(resultUpdateAll);
-      }
-
-      getAll().then(async (resultGetAll2) => {
-        // Elimina a todos los usuarios
-        for await(const person of resultGetAll2.rows) {
-          const resultDeleteAll = await deleteAll(person);
-
-          console.log(resultDeleteAll);
-        }
-      });
-    });
   });
+});
 
+document.getElementById("buttonShowAll").addEventListener("click", (event) => {
+  getAll().then((resultGetAll) => {
+    console.log(resultGetAll.rows);
+  });
+});
+
+document.getElementById("buttonUpdateAll").addEventListener("click", (event) => {
+  getAll().then((resultGetAll) => {
+    for (let person of resultGetAll.rows) {
+      person.doc.sync = true;
+      update(person.doc).then((result) => {
+        console.log(result);
+      }).catch((error) => console.error(error));
+    }
+  });
+});
+
+document.getElementById("buttonDeleteAll").addEventListener("click", (event) => {
+  getAll().then((resultGetAll) => {
+    for (let person of resultGetAll.rows) {
+      if (person.doc.sync) {        
+        deleteP(person.doc).then((result) => {
+          console.log(result);
+        }).catch((error) => console.error(error));
+      }
+    }
+  });
 });
